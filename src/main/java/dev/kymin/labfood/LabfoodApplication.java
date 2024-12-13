6a4c86e26907d5c9189b3b81d75af2b08f5eb5fe
Lim kyungmin <km8192@naver.com>
2024-12-14 01:25:10 +0900

    feat: project init

---
+++ src/main/java/dev/kymin/labfood/LabfoodApplication.java
@@ -0,0 +1,13 @@
+package dev.kymin.labfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kymin/labfood/LabfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kymin.labfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
